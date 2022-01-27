8008a4397d7e0a9caf565e9c0d222653c8662f78
Romario <morrisromo9966@gmail.com>
2022-01-27 18:20:03 -0500

    Manager prompts added to inquirer

--- index.js
+++ index.js
@@ -10,8 +10,23 @@ function prompt() {
         .prompt([
             {
                 type: 'input',
-                name: 'test',
-                message: 'This is a test prompt'
+                name: 'name',
+                message: 'What is your managers name?'
+            },
+            {
+                type: 'input',
+                name: 'ID',
+                message: 'What is your ID or badge number?'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'what is your email?'
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'What is your Github Username?'
             }
 
         ])
